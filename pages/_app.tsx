a111920ed5030de1b059febcec21bcfbe82064d5
Shah, Kabeer <Kabeer.Shah@vantive.com>
2025-09-01 00:15:42 +0530

    Fix SSR error by removing useRouter from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,8 @@
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import '../src/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Only run on client side
-    if (typeof window !== 'undefined') {
-      const { locale } = router;
-      // Set document direction for RTL languages
-      const isRTL = locale === 'ar' || locale === 'ur';
-      document.documentElement.dir = isRTL ? 'rtl' : 'ltr';
-      document.documentElement.lang = locale || 'en';
-    }
-  }, [router.locale]);
-
   return <Component {...pageProps} />;
 }
 
